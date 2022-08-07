884d284059e1c3e22e81aae2a86e4a52c16931a0
Gank <gasparlarrainv@gmail.com>
2022-08-07 16:28:06 -0400

    Working Version

--- opensea.js
+++ opensea.js
@@ -5,7 +5,7 @@ const { setTimeout } = require("timers/promises");
 
 Puppeteer.use(StealthPlugin())
 
-Puppeteer.launch({headless: false, args: [`--window-size=1280,1800`]}).then(async browser => {
+Puppeteer.launch({headless: false, args: ['--window-size=1280,1800', '--unlimited-storage', '--full-memory-crash-report']}).then(async browser => {
     const start = Date.now();
     const page = await browser.newPage();
     await page.setViewport({
@@ -13,15 +13,16 @@ Puppeteer.launch({headless: false, args: [`--window-size=1280,1800`]}).then(asyn
         height: 1800
     });
     // This is well explained in the API
-    await page.goto("https://opensea.io/collection/boredapeyachtclub", {"waitUntil" : "networkidle0"});
-    
+    await page.goto("https://opensea.io/collection/boredapeyachtclub?search[sortAscending]=true&search[sortBy]=CREATED_DATE", {"waitUntil" : "networkidle0"});
+    await page.waitForSelector('.cf-browser-verification', {hidden: true});
+
     const Images = await page.evaluate(async () => {
         var totalHeight = 0;
         var distance = window.innerHeight / 2; // should be less than or equal to window.innerHeight
         var IHref = [];
         await new Promise((resolve) => {
             window.scrollBy(0, 600);
-            const Timer = setInterval(async() => {
+             const Timer = setInterval(async() => {
                 if ( totalHeight <= document.body.scrollHeight - window.innerHeight) {
                     //Get Images
                     const ITag = document.querySelectorAll(".AssetMedia--img")
@@ -33,17 +34,15 @@ Puppeteer.launch({headless: false, args: [`--window-size=1280,1800`]}).then(asyn
                     }
                  }
                 else {
-                        if (totalHeight > document.body.scrollHeight - window.innerHeight) {
-                            clearInterval(Timer);
-                            clearInterval(scroll)
+                        clearInterval(Timer);
+                        clearInterval(scroll)
                             resolve("");
-                        }
                 }
-            }, 200);
+            }, 250);
             const scroll = setInterval(() => {
                 window.scrollBy(0, distance / 2);
                 totalHeight += distance / 2
-            },300)
+            },250)
         });
         return IHref;
     });
@@ -51,49 +50,10 @@ Puppeteer.launch({headless: false, args: [`--window-size=1280,1800`]}).then(asyn
     var end = Date.now() - start;
     fs.writeFile("firsttest.txt", Images.join(' \n') +  "\n" + end, function (err) {
         if (err) return console.log(err);
-        console.log('Error > nftsrc.txt');
+        console.log('Success');
       });
 
-    const Images2 = await page.evaluate(async (Images) => {
-        var totalHeight = 0;
-        var distance = window.innerHeight / 2; // should be less than or equal to window.innerHeight
-        var IHref = Images;
-        await new Promise((resolve) => {
-            window.scrollTo(0, 0);
-            const Timer = setInterval(async() => {
-                if ( totalHeight <= document.body.scrollHeight - window.innerHeight) {
-                    //Get Images
-                    const ITag = document.querySelectorAll(".AssetMedia--img")
-                    for (const tag of ITag) {
-                            const object = tag.firstChild.firstChild;
-                            if(!IHref.includes(object.src)) {
-                                IHref.push(object.src);                       
-                            }                       
-                    }
-                 }
-                else {
-                        if (totalHeight > document.body.scrollHeight - window.innerHeight) {
-                            clearInterval(Timer);
-                            clearInterval(scroll)
-                            resolve("");
-                        }
-                }
-            }, 100);
-            const scroll = setInterval(() => {
-                window.scrollBy(0, distance / 2);
-                totalHeight += distance / 2
-            },300)
-        });
-        return IHref;
-    }, Images);
-    await page.screenshot({path: "opensea.png"});
-    console.log(Images2);
-
-    console.log("done")
-
-    var end = Date.now() - start;
-    fs.writeFile("nftsrc.txt", Images2.join(' \n') +  "\n" + end, function (err) {
-        if (err) return console.log(err);
-        console.log('Error > nftsrc.txt');
-      });
+      await page.close()
 });
+await browser.close();
+
